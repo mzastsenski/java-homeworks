1650203ff654605fd71dbd0cacff872c7f3e57ff
koljan12 <koljan_z@yahoo.de>
2022-08-08 22:23:45 +0200

    Homework220808-2

---
+++ Homework220808/Homeworks220808_2.java
@@ -0,0 +1,18 @@
+package com.telran.berlin.homeworks.Homework220808;
+
+import java.util.Scanner;
+
+public class Homeworks220808_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter number1: ");
+        int num = sc.nextInt();
+
+        System.out.println("entrance: " + num);
+        int result = (int) (Math.random() * num + 1);
+        if (result == 1) result = 2;
+        System.out.println("We make a choice in the range 2 - " + num);
+        System.out.println("exit " + result);
+    }
+}
